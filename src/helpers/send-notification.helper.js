0d6606cb2b5d82cadc8b80f7cc72c4444af8092a
Hasan <hasanjonsadullayev97@gmail.com>
2025-03-20 00:16:31 +0500

    notification error handling

--- src/helpers/send-notification.helper.js
+++ src/helpers/send-notification.helper.js
@@ -3,8 +3,8 @@ import { messaging } from '../firebase/firebase.js';
 // Notifikatsiya jo'natish
 export const sendNotification = async (fcmToken, title, body, data) => {
   try {
-    if (!fcmToken || typeof fcmToken !== "string") {
-      console.warn("🚨 Warning: Invalid FCM token, skipping notification.");
+    if (!fcmToken || typeof fcmToken !== 'string') {
+      console.warn('🚨 Warning: Invalid FCM token, skipping notification.');
       return;
     }
 
@@ -15,14 +15,23 @@ export const sendNotification = async (fcmToken, title, body, data) => {
     };
 
     const result = await messaging.send(message);
-    console.log("✅ Notification sent successfully. ", result);
+    console.log('✅ Notification sent successfully. ', result);
   } catch (error) {
-    if (error.code === "messaging/invalid-argument") {
-      console.warn("⚠️ Warning: Invalid FCM token, skipping notification.");
-      return; // ❌ Xatoni `throw` qilmaydi, davom etadi
+    if (error.code === 'messaging/invalid-argument') {
+      console.warn(
+        `⚠️ Warning: Invalid FCM token: ${fcmToken}\n Error message: ${error.message}.`
+      );
+      return false; // ❌ Xatoni `throw` qilmaydi, davom etadi
     }
 
-    console.error("🚨 Error sending notification:", error);
+    if (error.code === 'messaging/registration-token-not-registered') {
+      console.error(
+        `❌  Warning: Invalid FCM token: ${fcmToken}\n Error message: ${error.message}.`
+      );
+      return false;
+    }
+
+    console.error('🚨 Error sending notification:', error);
     throw error; // ⚠️ Boshqa xatolik bo‘lsa, uni chiqaradi
   }
 };
