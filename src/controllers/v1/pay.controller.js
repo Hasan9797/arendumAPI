ac36a13117a52f3d572864e0c7dd212b7189c3b1
Hasan <hasanjonsadullayev97@gmail.com>
2025-03-23 21:17:18 +0500

    payment started

--- src/controllers/v1/pay.controller.js
+++ src/controllers/v1/pay.controller.js
@@ -1,54 +1,32 @@
 import axios from 'axios';
 
-const authAtmosAPI = async (req, res) => {
+const getAtmosToken = async (req, res) => {
   try {
-    const consumerKey = process.env.CONSUMER_KEY || 'your_consumer_key';
-    const consumerSecret =
-      process.env.CONSUMER_SECRET || 'your_consumer_secret';
-    const atmosAPIUrl = process.env.ATMOS_API_URL || 'your_atmos_api_url';
+    const consumerKey = process.env.CONSUMER_KEY;
+    const consumerSecret = process.env.CONSUMER_SECRET;
 
-    console.log('Atmos API URL:', atmosAPIUrl);
-
-    if (!consumerKey || !consumerSecret || !atmosAPIUrl) {
-      return res
-        .status(500)
-        .json({ error: 'Atmos API autentifikatsiya muvaffaqiyatsiz.' });
-    }
-
-    // Basic Authentication uchun `consumerKey:consumerSecret` ni Base64 formatga o‘girish
     const credentials = Buffer.from(
       `${consumerKey}:${consumerSecret}`
     ).toString('base64');
 
-    console.log('Atmos API Consumer Key:', consumerKey);
-    // Axios orqali so‘rov yuborish
     const response = await axios.post(
-      `${atmosAPIUrl}/token`,
-      'grant_type=client_credentials', // Form-data body
+      'https://partner.atmos.uz/token',
+      'grant_type=client_credentials',
       {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           Authorization: `Basic ${credentials}`,
         },
-        timeout: 5000, // 5 soniya timeout
       }
     );
 
-    console.log('Atmos API Access Token:', response.data.access_token);
-
-    res.status(200).json({
-      access_token: response.data.access_token,
-      expires_in: response.data.expires_in,
-      token_type: response.data.token_type,
-    });
+    res.status(200).json(response.data);
   } catch (error) {
-    console.error('Auth xatosi:', error.message);
-    res
-      .status(500)
-      .json({ error: 'Atmos API autentifikatsiya muvaffaqiyatsiz.' });
+    console.error('Auth Error:', error.response?.data || error.message);
+    res.status(500).json({ message: error.message });
   }
 };
 
-const payAtmosAPI = async (re, res) => {};
+const payAtmosAPI = async (req, res) => {};
 
-export default { authAtmosAPI, payAtmosAPI };
+export default { getAtmosToken, payAtmosAPI };
