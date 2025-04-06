b0dac26bde25b1f4d6b5a5e8c11a35bc04d81f19
Hasan <hasanjonsadullayev97@gmail.com>
2025-04-06 23:21:10 +0500

    card add and confirm test

--- src/services/pay/requests/card-confirm.request.js
+++ src/services/pay/requests/card-confirm.request.js
@@ -2,7 +2,8 @@ import AtmosApiService from '../atmos-api.service.js';
 
 class CardConfirmRequest extends AtmosApiService {
   constructor(transactionId, otpCode) {
-    super.setRoute('partner/bind-card/confirm').setParams({
+    super();
+    this.setRoute('partner/bind-card/confirm').setParams({
       transaction_id: transactionId,
       otp: otpCode,
     });
--- src/services/pay/requests/card-init.request.js
+++ src/services/pay/requests/card-init.request.js
@@ -2,7 +2,8 @@ import AtmosApiService from '../atmos-api.service.js';
 
 class CardInitRequest extends AtmosApiService {
   constructor(cardNumber, expiryDate) {
-    super.setRoute('partner/bind-card/init').setParams({
+    super();
+    this.setRoute('partner/bind-card/init').setParams({
       card_number: cardNumber,
       expiry: expiryDate,
     });
