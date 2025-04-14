960c17d4a879ba1605a932148fc57dc23ab0c0f3
Hasan <hasanjonsadullayev97@gmail.com>
2025-04-14 12:00:43 +0500

    payment test

--- src/services/pay/requests/payConfirmRequest.js
+++ src/services/pay/requests/payConfirmRequest.js
@@ -3,7 +3,7 @@ import AtmosApiService from '../atmosApi.service.js';
 class PayConfirmRequest extends AtmosApiService {
   constructor(transactionId) {
     super();
-    this.setRoute('merchant/pay/apply').setParams({
+    this.setRoute('merchant/pay/confirm').setParams({
       transaction_id: transactionId,
       otp: 111111,
       store_id: process.env.STORE_ID ?? 8032,
