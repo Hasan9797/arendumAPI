056633bf621c25d05f2d3a01f63fe0f047f87f0b
Hasan9797 <hasanjonsadullayev97@gmail.com>
2025-04-16 10:18:54 +0500

    migrate change

--- prisma/migrations/20250414235715_update_model/migration.sql
+++ prisma/migrations/20250414235715_update_model/migration.sql
@@ -1,11 +1,3 @@
--- DropIndex
-DROP INDEX "Structure_region_id_key";
-
--- DropIndex
-DROP INDEX "Transaction_region_id_key";
-
--- DropIndex
-DROP INDEX "Transaction_structure_id_key";
 
 -- AlterTable
 ALTER TABLE "Transaction" ADD COLUMN     "partner_transaction_id" INTEGER;
