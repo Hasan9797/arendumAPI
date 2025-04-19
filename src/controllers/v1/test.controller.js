4c0703c989997d4ddb1facab5886a4780de112b2
Hasan <hasanjonsadullayev97@gmail.com>
2025-04-19 22:35:10 +0500

    test route

--- src/controllers/v1/test.controller.js
+++ src/controllers/v1/test.controller.js
@@ -2,7 +2,14 @@ import { sendNotification } from '../../helpers/sendNotificationHelper';
 
 export const test = async (req, res) => {
   try {
-    await sendNotification(driver?.fcmToken, title, body, data);
+    const title = 'New Order';
+    const body = 'You have a new order';
+    const data = {
+      key: 'new_order',
+      order: JSON.stringify(req.body),
+    };
+
+    await sendNotification(req.body.fcmToken, title, body, data);
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(500).json({ success: false, error: error });
