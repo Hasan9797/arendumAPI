c1db6f376eb4d37f149890d0d7236f2b4877488e
Hasan <hasanjonsadullayev97@gmail.com>
2025-05-04 22:36:49 +0500

    optimize auth

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   console.error('Global error handler:', err);
 
-  const statusCode =
-    err.status && typeof err.status === 'number' ? err.status : 500;
+  const statusCode = err.code && typeof err.code === 'number' ? err.code : 500;
 
   res.status(statusCode).json({
     success: false,
