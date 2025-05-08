02204ccfeacd943a4bdae6e8e155400ff4a45ffd
Hasan9797 <hasanjonsadullayev97@gmail.com>
2025-05-08 21:20:44 +0500

    sms

--- src/services/sms/requests/authTokenRequest.js
+++ src/services/sms/requests/authTokenRequest.js
@@ -1,19 +1,21 @@
 import { FormData } from 'formdata-node';
 import AxiosApiService from '../sendRequestSmsApi.service.js';
 
+var data = new FormData();
+data.append('email', process.env.ESKIZ_LOGIN);
+data.append('password', process.env.ESKIZ_PASSWORD);
+
 export class AuthTokenRequest extends AxiosApiService {
   constructor() {
-    this.setRoute('auth/login')
-      .setRequest({
-        method: 'post',
-        maxBodyLength: Infinity,
-        route: 'auth/login',
-        headers: {
-          ...data.getHeaders(),
-        },
-        data: data,
-      })
-      .send();
+    this.setRequest({
+      method: 'post',
+      maxBodyLength: Infinity,
+      route: 'auth/login',
+      headers: {
+        ...data.getHeaders(),
+      },
+      data: data,
+    }).send();
 
     return this;
   }
