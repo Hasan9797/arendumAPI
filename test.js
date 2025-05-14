60367977d30e73ed95c1c5a9c5d677ede0540c5e
HaSaN_JoN <91729142+Hasan9797@users.noreply.github.com>
2025-05-14 11:46:09 +0500

    Update test.js

--- test.js
+++ test.js
@@ -4,23 +4,5 @@ import dotenv from 'dotenv';
 import redisClient from './src/config/redis.js';
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET || 'secret-access-key';
-function errorThrow() {
-  const error = new Error('Test Error');
-  error.code = 400;
-  throw error;
-}
-
-function getError() {
-  try {
-    errorThrow();
-  } catch (error) {
-    console.log(error.message, error.code);
-  }
-}
-
  const cacheKey = `eskizToken`;
     await redisClient.del(cacheKey);
-
-// getError();
-
